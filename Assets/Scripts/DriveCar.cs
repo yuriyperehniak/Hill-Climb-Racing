07ffc2ce5a1392580af5d2329e51a7b9c511d415
Yuriy Perehniak <yuraperehniak@gmail.com>
2023-08-28 19:31:02 +0300

    fix script

--- Assets/Scripts/DriveCar.cs
+++ Assets/Scripts/DriveCar.cs
@@ -11,7 +11,7 @@ public class DriveCar : MonoBehaviour
     [SerializeField] private GameObject _brakeButton;
     [SerializeField] private bool _isGasPressed = false;
     [SerializeField] private bool _isBrakePressed = false;
-    private int _currentDirection = 0; // -1 для руху назад, 0 для стоянки, 1 для руху вперед
+    private int _currentDirection = 0;
 
     private void Update()
     {
@@ -53,9 +53,8 @@ public class DriveCar : MonoBehaviour
         float moveInput = _currentDirection;
         _frontTireRB.AddTorque(-moveInput * _speed * Time.deltaTime);
         _backTireRB.AddTorque(-moveInput * _speed * Time.deltaTime);
-         float rotationInput = Input.GetAxis("Horizontal");
-        _carRB.angularVelocity = -rotationInput * _rotationSpeed * Mathf.Deg2Rad;
-    }
+        _carRB.AddTorque(-moveInput * _rotationSpeed * Time.fixedDeltaTime);
+        }
 
     public bool GetGasPedalState()
     {
